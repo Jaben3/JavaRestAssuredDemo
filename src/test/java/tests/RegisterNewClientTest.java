ca157199cf94f8c879b2913029b6e2cc1306c710
Jaben3 <benduch3@gmail.com>
2023-07-22 13:40:18 +0200

    Added class to generate new API Token (RegisterNewClientTest)

---
+++ src/test/java/tests/RegisterNewClientTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static io.restassured.RestAssured.*;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class RegisterNewClientTest extends BaseTest{
+
+    @Test
+    public void registerApi(){
+        JSONObject body = new JSONObject();
+        body.put("clientName", "TestJB2");
+        body.put("clientEmail", "TestJB2@example.com");
+
+        given()
+                .body(body.toString())
+                .contentType("application/json")
+        .when()
+                .post("/api-clients")
+        .then()
+                .log().all()
+                .statusCode(201);
+
+
+        given()
+                .body(body.toString())
+                .contentType("application/json")
+        .when()
+                .post("/api-clients")
+        .then()
+                .log().all()
+                .statusCode(409);
+
+
+    }
+}
